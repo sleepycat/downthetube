78619cc982410ad4116581489959b160c73e36f5
Mike Williamson <blessedbyvirtuosity@gmail.com>
2011-12-16 21:19:10 -0500

    Refactored playlist tests
    Rethinking the spec for the playlist class in hopes of making them a bit
    more robust.

--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
 
 describe Youtube::Playlist do
+  before do
+
+    @doc = REXML::Document.new(File.read("spec/feeds/historia_and_documentario_playlist.xml")).root
+
+    @response = double(GData::HTTP::Response, :to_xml => @doc)
+    @gd_client = double(GData::Client::YouTube, :get => @response)
+
+    @pl = Youtube::Playlist.new("281715DB20526F2B")
+    @pl.instance_variable_set(:@client, @gd_client)
+  end
 
   it "can be initalized with a playlist id" do
-    @doc = REXML::Document.new(File.read("spec/feeds/historia_and_documentario_playlist.xml")).root
-    response = double(GData::HTTP::Response, :to_xml => @doc)
-    gd = double(GData::Client::YouTube, :get => response)
-    GData::Client::YouTube.should_receive(:new).and_return(gd)
-
-    pl = Youtube::Playlist.new("281715DB20526F2B")
-    pl.title.should eql("Historia & Documentario")
-    pl.id.should eql("281715DB20526F2B")
+    @gd_client.should_receive(:get)
+    .with("http://gdata.youtube.com/feeds/api/playlists/281715DB20526F2B")
+    @pl.title.should eql("Historia & Documentario")
+    @pl.id.should eql("281715DB20526F2B")
   end
 
   it "can be initialized with a rexml element" do 
@@ -22,23 +28,13 @@ describe Youtube::Playlist do
   end
 
   it "returns an array of videos" do
-    @doc = REXML::Document.new(File.read("spec/feeds/historia_and_documentario_playlist.xml")).root
-    response = double(GData::HTTP::Response, :to_xml => @doc)
-    gd = double(GData::Client::YouTube, :get => response)
-    GData::Client::YouTube.should_receive(:new).and_return(gd)
     # return the first entry element from doc:
-    pl = Youtube::Playlist.new(@doc.elements['entry'])
-    pl.videos.should have(19).videos
+    @pl.videos.should have(19).videos
   end
 
   it "limits the number of videos returned" do
-    @doc = REXML::Document.new(File.read("spec/feeds/historia_and_documentario_playlist.xml")).root
-    response = double(GData::HTTP::Response, :to_xml => @doc)
-    gd = double(GData::Client::YouTube, :get => response)
-    GData::Client::YouTube.should_receive(:new).and_return(gd)
     # return the first entry element from doc:
-    pl = Youtube::Playlist.new(@doc.elements['entry'])
-    pl.videos(5).should have(5).videos
+    @pl.videos(5).should have(5).videos
   end
 
 end
